763104811c1c07aa0c815f16d7094011a40edab5
uleeankin <sokolowa.yu.a@gmail.com>
2022-09-08 11:29:39 +0300

    init project commit

---
+++ src/main/java/ru/rsreu/photostudio/PhotostudioApplication.java
@@ -0,0 +1,13 @@
+package ru.rsreu.photostudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotostudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotostudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rsreu/photostudio/PhotostudioApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rsreu.photostudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotostudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
